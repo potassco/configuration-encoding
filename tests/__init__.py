46170f4e90b520fa1290d1b921cdc621432ab60f
Nicolas Rühling <nicolas.ruehling@uni-potsdam.de>
2023-07-06 14:49:15 +0200

    more tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,21 +9,25 @@ from clingo.control import Control
 class Solver():
 
     def __init__(self, options=()):
-        self.ctl = Control(list(options))
+        self.ctl = Control(['--opt-mode=enum', '0'] + list(options))
 
-    def solve(self, files):
-        for f in files:
-            self.ctl.load(f)
+    def solve(self, f):
+        self.ctl.load('encoding.lp')
+        if f.endswith('.lp'):
+            self.ctl.load(f'tests/instances/{f}')
+        else:
+            self.ctl.add(f)
+
         self.ctl.ground([("base", [])])
 
         ret = []
         self.ctl.solve(on_model=lambda m: ret.append(
             [str(sym) for sym in m.symbols(shown=True)]))
-
+        ret.sort()
         return ret
 
 
-def solve(files, options=()):
+def solve(f, options=()):
     solver = Solver(options)
-    ret = solver.solve(files)
+    ret = solver.solve(f)
     return ret
--- tests/test_clingo.py
+++ tests/test_clingo.py
@@ -7,23 +7,34 @@ import unittest
 
 class TestMain(unittest.TestCase):
 
+    def test_object(self):
+        self.assertEqual(solve('type(a).'), [['selected((),a)']])
+        self.assertEqual(solve('type(a). type(b).'), [])
+
+        self.assertEqual(
+            solve('type(a). type(b). part(a,b,b). multiplicity(a,b,b,1).'),
+            [['selected((),a)', 'selected((b,((),0)),b)']])
+        self.assertEqual(
+            solve(
+                'type(a). type(b). part(a,b,b). multiplicity(a,b,b,1). multiplicity(a,b,b,2).'
+            ), [['selected((),a)', 'selected((b,((),0)),b)'],
+                [
+                    'selected((),a)', 'selected((b,((),0)),b)',
+                    'selected((b,((),1)),b)'
+                ]])
+
     def test_connection(self):
-        self.assertEqual(
-            len(
-                solve(['encoding.lp', 'tests/instances/connection_port.lp'],
-                      ['--opt-mode=enum', '0', '-c', 'num_hdd=1'])), 3)
+        self.assertEqual(len(solve('connection.lp', ['-c', 'num_hdd=1'])), 3)
 
-        self.assertEqual(
-            len(
-                solve(['encoding.lp', 'tests/instances/connection_port.lp'],
-                      ['--opt-mode=enum', '0', '-c', 'num_hdd=2'])), 8)
+        self.assertEqual(len(solve('connection.lp', ['-c', 'num_hdd=2'])), 8)
 
-        self.assertEqual(
-            len(
-                solve(['encoding.lp', 'tests/instances/connection_port.lp'],
-                      ['--opt-mode=enum', '0', '-c', 'num_hdd=3'])), 17)
+        self.assertEqual(len(solve('connection.lp', ['-c', 'num_hdd=3'])), 17)
 
+        self.assertEqual(len(solve('connection.lp', ['-c', 'num_hdd=4'])), 34)
+
+    def test_table_constraint(self):
         self.assertEqual(
-            len(
-                solve(['encoding.lp', 'tests/instances/connection_port.lp'],
-                      ['--opt-mode=enum', '0', '-c', 'num_hdd=4'])), 34)
+            solve('tb_basic.lp'),
+            [['selected((),a)', 'val(((),b),"b1")', 'val(((),c),"c1")'],
+             ['selected((),a)', 'val(((),b),"b2")', 'val(((),c),"c2")']])
+        self.assertEqual(len(solve('tb_colors.lp')), 8)
