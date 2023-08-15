3fc456a9c336eee059007661ad4f5d687bcf703d
Nicolas Rühling <nicolas.ruehling@uni-potsdam.de>
2023-08-15 12:22:41 +0200

    adapted viz model to fclingo encodings

--- make_plots.sh
+++ make_plots.sh
@@ -8,7 +8,14 @@ do
         outdir=examples/$ex
         model=$outdir/model.lp
         echo "Running for ${ex} example..."
-        clingraph $model --viz-encoding $viz_file --out=render --type=digraph --dir=$outdir --name-format=clingraph --format=png
+        clingraph $model --viz-encoding $viz_file --out=render --type=digraph --dir=$outdir --name-format=model --format=png
+    fi
+    if [ -d examples/$ex ] && [ -e examples/$ex/fmodel.lp ]
+    then
+        outdir=examples/$ex
+        model=$outdir/fmodel.lp
+        echo "Running for ${ex} example..."
+        clingraph $model --viz-encoding $viz_file --out=render --type=digraph --dir=$outdir --name-format=fmodel --format=png
     fi
 done
 #
