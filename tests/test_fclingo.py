b49ac3ce374c2ffc94a07343fcce7e3ad2d964dc
Nicolas Rühling <nicolas@ruehling.net>
2023-11-16 16:25:03 +0100

    adapting fclingo tests to using API

--- tests/test_fclingo.py
+++ tests/test_fclingo.py
@@ -49,16 +49,20 @@ class TestMain(TestCase):
             ]])
 
     def test_connection(self):
-        self.assertEqual(len(fsolve('connection.lp', ['-c', 'num_hdd=1'])), 3)
-
-        self.assertEqual(len(fsolve('connection.lp', ['-c', 'num_hdd=2'])), 8)
-
-        self.assertEqual(len(fsolve('connection.lp', ['-c', 'num_hdd=3'])), 17)
-
-        self.assertEqual(len(fsolve('connection.lp', ['-c', 'num_hdd=4'])), 34)
+        self.assertEqual(
+            len(fsolve('connection.lp', options=['-c', 'num_hdd=1'])), 3)
 
         self.assertEqual(
-            fsolve('connection_constraint.lp', []),
+            len(fsolve('connection.lp', options=['-c', 'num_hdd=2'])), 8)
+
+        self.assertEqual(
+            len(fsolve('connection.lp', options=['-c', 'num_hdd=3'])), 17)
+
+        self.assertEqual(
+            len(fsolve('connection.lp', options=['-c', 'num_hdd=4'])), 34)
+
+        self.assertEqual(
+            fsolve('connection_constraint.lp'),
             [[
                 'connected(((b,0),((),0)),((b,0),((),0)),previous)',
                 'connected(((b,1),((),0)),((b,0),((),0)),previous)',
@@ -81,58 +85,51 @@ class TestMain(TestCase):
         self.assertEqual(fsolve('simple_sum.lp'),
                          [[
                              'selected((),a)', 'selected((b,((),0)),b)',
-                             'selected((b,((),1)),b)', 'val(((),s),2)',
-                             'val(((b,((),0)),c),1)', 'val(((b,((),1)),c),1)'
+                             'selected((b,((),1)),b)', ('((),s)', 2),
+                             ('((b,((),0)),c)', 1), ('((b,((),1)),c)', 1)
                          ],
                           [
                               'selected((),a)', 'selected((b,((),0)),b)',
-                              'selected((b,((),1)),b)', 'val(((),s),3)',
-                              'val(((b,((),0)),c),1)', 'val(((b,((),1)),c),2)'
+                              'selected((b,((),1)),b)', ('((),s)', 3),
+                              ('((b,((),0)),c)', 1), ('((b,((),1)),c)', 2)
                           ],
                           [
                               'selected((),a)', 'selected((b,((),0)),b)',
-                              'selected((b,((),1)),b)', 'val(((),s),3)',
-                              'val(((b,((),0)),c),2)', 'val(((b,((),1)),c),1)'
+                              'selected((b,((),1)),b)', ('((),s)', 3),
+                              ('((b,((),0)),c)', 2), ('((b,((),1)),c)', 1)
                           ],
                           [
                               'selected((),a)', 'selected((b,((),0)),b)',
-                              'selected((b,((),1)),b)', 'val(((),s),4)',
-                              'val(((b,((),0)),c),2)', 'val(((b,((),1)),c),2)'
+                              'selected((b,((),1)),b)', ('((),s)', 4),
+                              ('((b,((),0)),c)', 2), ('((b,((),1)),c)', 2)
                           ]])
 
-        self.assertEqual(fsolve('simple_count.lp'), [[
-            'selected((),a)', 'selected((b,((),0)),b)',
-            'selected((b,((),1)),b)', 'val(((),c),2)'
-        ], ['selected((),a)', 'selected((b,((),0)),b)', 'val(((),c),1)'
-            ], ['selected((),a)', 'val(((),c),0)']])
+        self.assertEqual(
+            fsolve('simple_count.lp'),
+            [['selected((),a)', ('((),c)', 0)],
+             ['selected((),a)', 'selected((b,((),0)),b)', ('((),c)', 1)],
+             [
+                 'selected((),a)', 'selected((b,((),0)),b)',
+                 'selected((b,((),1)),b)', ('((),c)', 2)
+             ]])
 
-        self.assertEqual(fsolve('simple_min.lp'), [[
-            'selected((),a)', 'val(((),b),1)', 'val(((),c),2)', 'val(((),m),1)'
-        ], [
-            'selected((),a)', 'val(((),b),1)', 'val(((),c),3)', 'val(((),m),1)'
-        ], [
-            'selected((),a)', 'val(((),b),2)', 'val(((),c),2)', 'val(((),m),2)'
-        ], [
-            'selected((),a)', 'val(((),b),2)', 'val(((),c),3)', 'val(((),m),2)'
-        ], [
-            'selected((),a)', 'val(((),b),3)', 'val(((),c),2)', 'val(((),m),2)'
-        ], [
-            'selected((),a)', 'val(((),b),3)', 'val(((),c),3)', 'val(((),m),3)'
-        ]])
+        self.assertEqual(
+            fsolve('simple_min.lp'),
+            [['selected((),a)', ('((),b)', 1), ('((),c)', 2), ('((),m)', 1)],
+             ['selected((),a)', ('((),b)', 1), ('((),c)', 3), ('((),m)', 1)],
+             ['selected((),a)', ('((),b)', 2), ('((),c)', 2), ('((),m)', 2)],
+             ['selected((),a)', ('((),b)', 2), ('((),c)', 3), ('((),m)', 2)],
+             ['selected((),a)', ('((),b)', 3), ('((),c)', 2), ('((),m)', 2)],
+             ['selected((),a)', ('((),b)', 3), ('((),c)', 3), ('((),m)', 3)]])
 
-        self.assertEqual(fsolve('simple_max.lp'), [[
-            'selected((),a)', 'val(((),b),1)', 'val(((),c),2)', 'val(((),m),2)'
-        ], [
-            'selected((),a)', 'val(((),b),1)', 'val(((),c),3)', 'val(((),m),3)'
-        ], [
-            'selected((),a)', 'val(((),b),2)', 'val(((),c),2)', 'val(((),m),2)'
-        ], [
-            'selected((),a)', 'val(((),b),2)', 'val(((),c),3)', 'val(((),m),3)'
-        ], [
-            'selected((),a)', 'val(((),b),3)', 'val(((),c),2)', 'val(((),m),3)'
-        ], [
-            'selected((),a)', 'val(((),b),3)', 'val(((),c),3)', 'val(((),m),3)'
-        ]])
+        self.assertEqual(
+            fsolve('simple_max.lp'),
+            [['selected((),a)', ('((),b)', 1), ('((),c)', 2), ('((),m)', 2)],
+             ['selected((),a)', ('((),b)', 1), ('((),c)', 3), ('((),m)', 3)],
+             ['selected((),a)', ('((),b)', 2), ('((),c)', 2), ('((),m)', 2)],
+             ['selected((),a)', ('((),b)', 2), ('((),c)', 3), ('((),m)', 3)],
+             ['selected((),a)', ('((),b)', 3), ('((),c)', 2), ('((),m)', 3)],
+             ['selected((),a)', ('((),b)', 3), ('((),c)', 3), ('((),m)', 3)]])
 
         self.assertEqual(
             fsolve('count_at_optional.lp'),
@@ -140,7 +137,11 @@ class TestMain(TestCase):
              [
                  'selected((),a)', 'selected((b,((),0)),b)',
                  'selected((c,((b,((),0)),0)),c)', 'val(((b,((),0)),cou),1)'
-             ]])
+             ]], [['selected((),a)', ('((b,((),0)),cou)', 0)],
+                  [
+                      'selected((),a)', 'selected((b,((),0)),b)',
+                      'selected((c,((b,((),0)),0)),c)', ('((b,((),0)),cou)', 1)
+                  ]])
 
     def test_constraint(self):
         self.assertEqual(
